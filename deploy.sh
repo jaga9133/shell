548046dc52f13bc25233ac4af7441e0efbf000a0
jaga9133 <85977422+jaga9133@users.noreply.github.com>
2021-07-07 08:07:41 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "we  are doing deployment"
-wget --user=jaga --password=jaga http://18.118.141.69:8081/nexus/service/local/repositories/jaga/content/java/wwp/11/wwp-11.war
+wget --user=jaga --password=$1 http://18.118.141.69:8081/nexus/service/local/repositories/jaga/content/java/wwp/11/wwp-11.war
