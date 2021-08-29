05716ac1bf7cd183209b41879a21280046e3d3d9
jaga9133 <85977422+jaga9133@users.noreply.github.com>
2021-08-29 12:43:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "we  are doing deployment"
-wget --user=jaga --password=jaga http://3.144.101.143:8081/nexus/service/local/repositories/jaga/content/jaggu/java/10/java-10.war
+wget --user=jaga --password=$1 http://3.144.101.143:8081/nexus/service/local/repositories/jaga/content/jaggu/java/10/java-10.war
